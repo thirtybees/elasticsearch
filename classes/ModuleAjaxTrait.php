faa2484492bc8e45b674668d01beca764dbecafe
Michael Dekker <git@michaeldekker.nl>
2017-11-09 14:16:57 +0100

    Another fix for saving servers

--- classes/ModuleAjaxTrait.php
+++ classes/ModuleAjaxTrait.php
@@ -61,10 +61,9 @@ trait ModuleAjaxTrait
                 if ($setting === static::METAS) {
                     Meta::saveMetas($value);
                     continue;
-                } elseif ($setting === static::SERVERS) {
-                    // If proxied, force read and write to 'yes'
+                } elseif ($setting == static::SERVERS) {
                     if ($settings[static::PROXY]) {
-                        foreach ($value as $server) {
+                        foreach ($value as &$server) {
                             $server['read'] = 1;
                             $server['write'] = 1;
                         }
